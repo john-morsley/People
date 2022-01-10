ead19ad0eb28a94d91e429fd5ac0a9115eb8068b
John Morsley <johnmorsley@gmail.com>
2022-01-10 17:23:17 +0000

    Added domain model ToString unit tests.

--- tests/UnitTests/Domain.Models.Tests/UserTests.cs
+++ tests/UnitTests/Domain.Models.Tests/UserTests.cs
@@ -5,19 +5,39 @@ public class UserTests
     [Test]
     public void EmptyUserToString()
     {
-        var user = new User();
+        // Arrange...
+        var userId = Guid.NewGuid();
+        var user = new User()
+        {
+            Id = userId,
+        };
+
+        // Act...
         var result = user.ToString();
+
+        // Assert...
+        result.Should().Be($"Id: {userId} | FirstName: [Null] | LastName: [Null] | Sex: [Null] | Gender: [Null] | DateOfBirth: [Null]");
     }
 
     [Test]
     public void CompleteUserToString()
     {
-        var user = new User();
-        user.FirstName = "John";
-        user.LastName = "Morsley";
-        user.Sex = Sex.Male;
-        user.Gender = Gender.Cisgender;
-        user.DateOfBirth = new DateTime(2000, 4, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        // Arrange...
+        var userId = Guid.NewGuid();
+        var user = new User
+        {
+            Id = userId,
+            FirstName = "John",
+            LastName = "Doe",
+            Sex = Sex.Male,
+            Gender = Gender.Cisgender,
+            DateOfBirth = new DateTime(2000, 4, 1, 0, 0, 0, 0, DateTimeKind.Utc)
+        };
+
+        // Act...
         var result = user.ToString();
+
+        // Assert...
+        result.Should().Be($"Id: {userId} | FirstName: John | LastName: Doe | Sex: Male | Gender: Cisgender | DateOfBirth: 2000-04-01");
     }
 }
