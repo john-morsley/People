cc286ca6632a7907f99351b620b7450d76b76137
John Morsley <johnmorsley@gmail.com>
2022-04-01 00:40:04 +0100

    Added some metrics to the Synchronizer application.

--- src/Infrastructure/Morsley.UK.People.Synchronizer/Worker.cs
+++ src/Infrastructure/Morsley.UK.People.Synchronizer/Worker.cs
@@ -1,21 +1,51 @@
+using MongoDB.Driver;
+using Morsley.UK.People.Domain.Models;
+
 namespace Morsley.UK.People.Synchronizer;
 
 public class Worker : BackgroundService
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger _logger;
 
-    public Worker(ILogger logger)
+    public Worker(IConfiguration configuration, ILogger logger)
     {
+        _configuration = configuration;
         _logger = logger;
     }
 
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var readSettings = GetReadSettings();
+        var writeSettings = GetWriteSettings();
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.Information("Worker running at: {time}", DateTimeOffset.Now);
+            var read = NumberOfPeopleInDatabase(readSettings);
+            var write = NumberOfPeopleInDatabase(writeSettings);
+            _logger.Information("Read: {read} | Write: {write}", read, write);
             await Task.Delay(1000, stoppingToken);
         }
+    }
+
+    private (string Username, string Password, string Port, string Database, string Table) GetSettings(string section)
+    {
+        var username = _configuration[$"{section}MongoDBSettings:Username"];
+        var password = _configuration[$"{section}MongoDBSettings:Password"];
+        var port = _configuration[$"{section}MongoDBSettings:Port"];
+        var database = _configuration[$"{section}MongoDBSettings:DatabaseName"];
+        var table = _configuration[$"{section}MongoDBSettings:TableName"];
+        return (username, password, port, database, table);
+    }
+
+    private (string Username, string Password, string Port, string Database, string Table) GetWriteSettings()
+    {
+        return GetSettings("Write");
+    }
+
+    private (string Username, string Password, string Port, string Database, string Table) GetReadSettings()
+    {
+        return GetSettings("Read");
     }
 
     public override Task StartAsync(CancellationToken cancellationToken)
@@ -26,5 +56,20 @@ public class Worker : BackgroundService
     public override Task StopAsync(CancellationToken cancellationToken)
     {
         return base.StopAsync(cancellationToken);
+    }
+
+    public string GetConnectionString(string username, string password, string port)
+    {
+        return $"mongodb://{username}:{password}@localhost:{port}";
+    }
+
+    private long NumberOfPeopleInDatabase((string Username, string Password, string Port, string Database, string Table) settings)
+    {
+        var connectionString = GetConnectionString(settings.Username, settings.Password, settings.Port);
+        var mongoClient = new MongoClient(connectionString);
+        var database = mongoClient.GetDatabase(settings.Database);
+        var peopleTable = database.GetCollection<Person>(settings.Table);
+        var numberOfPeople = peopleTable.Find(_ => true).CountDocuments();
+        return numberOfPeople;
     }
 }
